cffedc9f68bc1a3853d23d13eb45183e495eca1b
Kutay Akgun <kutay.akgun@gmail.com>
2022-04-14 13:10:34 +0300

    comment fixed

--- Projects/03/p_02.c
+++ Projects/03/p_02.c
@@ -3,17 +3,17 @@
  * Write a program that formats product information entered by the user. 
  * A session with the program should look like this:
  *
- *		Enter item number: 583
- *		Enter unit price: 13.5
- *		Enter purchase date (mm/dd/yyyy): 10/24/2010
+ *	Enter item number: 583
+ *	Enter unit price: 13.5
+ *	Enter purchase date (mm/dd/yyyy): 10/24/2010
  *
- *		Item			Unit			Purchase
- *						Price			Date
- *		583			$  13.50		10/24/2010
+ *	Item		Unit			Purchase
+ *				Price			Date
+ *	583			$  13.50		10/24/2010
  *
- *	The item number and date should be left justified; the unit price
- *	should be right justified. Allow dollar amounts up to $9999.90. 
- *	Hint: Use tabs to line up the columns.
+ * The item number and date should be left justified; the unit price
+ * should be right justified. Allow dollar amounts up to $9999.90. 
+ * Hint: Use tabs to line up the columns.
  *
  */
 
