ffc1233fe52c2e5ae44dc678d749a247d900b84c
Kutay Akgun <kutay.akgun@gmail.com>
2022-04-14 03:01:10 +0300

    fix the comment

--- Projects/02/p_01.c
+++ Projects/02/p_01.c
@@ -2,12 +2,12 @@
  * Write a program that uses printf to display the
  * following picture on the screen:
  *
- *					*
- *				  *
- *				 *
- *		*		*
- *		  *  *
  *			 *
+ *			*
+ *		       *
+ *		*     *
+ *		  *  *
+ *	           *
  */
 
 #include <stdio.h>
