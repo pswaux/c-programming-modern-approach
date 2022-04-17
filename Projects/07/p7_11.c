e53145b00c7925f4410e63e78b1fb0addcfd5e62
pswaux <kutay.akgun@gmail.com>
2022-04-17 13:18:40 +0300

    p7_11.c added

---
+++ Projects/07/p7_11.c
@@ -0,0 +1,33 @@
+/**
+ *
+ * Write a program that takes a first name and last name
+ * entered by the user and displays the last name, a comma,
+ * and the first initial, followed by a period:
+ * 
+ *    Enter a first and last name: Lloyd Fosdick
+ *    Fosdick, L.
+ *
+ * The user's input may contain extra spaces before the first name,
+ * between the first and last names, and after the last name.
+ * 
+ */
+#include <stdio.h>
+
+int main(void) {
+   char first, last;
+   int len = 0, space;
+   
+   printf("Enter a first and last name: ");
+   scanf(" %c", &first);      // get the first character
+   while (getchar() != ' ');  // and skip the rest of the characters
+
+   scanf(" %c", &last);
+   while (last != '\n') {
+      putchar(last);
+      scanf("%c", &last);
+   }
+
+   printf(", %c.\n", first);
+
+   return 0;
+}
