bdface5aaebad129b39d858b57d5e406cfeded3d
pswaux <kutay.akgun@gmail.com>
2022-05-12 15:07:41 +0300

    p12_04.c added

---
+++ Projects/12/p12_04.c
@@ -0,0 +1,56 @@
+/**
+ *
+ * (a) Write a program that reads a message, then checks whether it's a palindrome
+ * (the letters in the message are the same from left to right as from right to left):
+ *
+ *    Enter a message: He lived as a devil, eh?
+ *    Palindrome
+ *
+ *    Enter a message: Madam, I am Adam.
+ *    Not a palindrome
+ *
+ * Ignore all characters that aren't letters. Use integer variables to keep track of
+ * positions in the array.
+ *
+ * (b) Revise the program to use pointers instead of integers to keep track of
+ * positions in the array
+ *
+ */
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define SIZE 80
+
+int main(void) {
+   char message[SIZE];
+   char ch, *ptr_first, *ptr_last;
+   int length = 0;
+   bool isPalindrome = false;
+
+   printf("Enter a message: ");
+   for (; ;) {
+      ch = getchar();
+
+      if (ch == '\n' || length == SIZE) break;
+
+      if (isalpha(ch))
+         message[length++] = tolower(ch);
+   }
+
+   for (ptr_first = message, ptr_last = message + length - 1; ptr_first < ptr_last; ptr_first++, ptr_last--) {
+      if (*ptr_first == *ptr_last)
+         isPalindrome = true;
+      else {
+         isPalindrome = false;
+         break;
+      }
+   }
+
+   if (isPalindrome)
+      printf("Palindrome\n");
+   else
+      printf("Not a palindrome\n");
+
+   return 0;
+}
