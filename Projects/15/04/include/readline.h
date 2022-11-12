8a676035b3a60c788aa1ca9c41ca2bbd51a46534
pswaux <kutay.akgun@gmail.com>
2022-11-13 00:32:00 +0300

    project 4 added

---
+++ Projects/15/04/include/readline.h
@@ -0,0 +1,10 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+#define MAX_REMIND 50   /* maximum number of reminders      */
+#define MSG_LEN 60      /* max length of reminder message   */
+
+/* prototypes  */
+int read_line(char str[], int n);
+
+#endif
